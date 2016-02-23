e3fdca1cffb505da94eb7d013ca09aaf06c025ea
mlabouardy <mohamed.labouardy@gmail.com>
2016-02-23 20:16:44 +0100

    basic spring boot project

---
+++ src/test/java/com/labouardy/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,18 @@
+package com.labouardy.socialnetwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SocialNetworkApplication.class)
+@WebAppConfiguration
+public class SocialNetworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
